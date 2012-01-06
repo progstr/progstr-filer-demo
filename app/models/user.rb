143c98f4fa91c2ff324ac7613872712a7bbec0fa
Hristo Deshev <hristo@deshev.com>
2012-01-06 15:26:25 +0200

    Upgrade to progstr-filer 0.8.1 and set up User avatar validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   has_file :avatar, AvatarUploader
+  validates_presence_of :avatar
+  validates_file_size_of :avatar, :less_than => 1 * 1024 * 1024
+  validates_file_extension_of :avatar, :allowed => ["jpg", "png"]
 end
